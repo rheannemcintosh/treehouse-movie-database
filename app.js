fc625f485aa01d1ff60bec033c8e0bde3e33bb5b
rheannemcintosh <rheanne.mcintosh@abdn.ac.uk>
2021-01-23 21:28:36 +0000

    Refactor code

--- app.js
+++ app.js
@@ -1,28 +1,17 @@
-// Include Sequelize
-const Sequelize = require('sequelize');
-const sequelize = new Sequelize({
-    dialect: 'sqlite',
-    storage: 'movies.db',
-    logging: true,
-});
-
-//Movie Model
-class Movie extends Sequelize.Model {}
-Movie.init({
-    title: Sequelize.STRING
-}, { sequelize });
+const db = require('./db');
+const { Movie } = db.models;
 
 (async () => {
-    await sequelize.sync({ force: true });
+    await db.sequelize.sync({ force: true });
+
     try {
-        const movie = await Movie.create({
+        await Movie.create({
             title: 'Toy Story',
         });
-        const movie2 = await Movie.create({
+        await Movie.create({
             title: 'The Incredibles',
         });
-        //await sequelize.authenticate();
-        //console.log('Connection to the DB successful!');
+        
     } catch (error) {
         console.log('Error connecting to the DB: ', error);
     }
---
+++ db/index.js
@@ -0,0 +1,21 @@
+// Require Sequelize
+const Sequelize = require('sequelize');
+
+// Configure the Sequelize instance
+const sequelize = new Sequelize({
+  dialect: 'sqlite',
+  storage: 'movies.db'
+});
+
+// Configure the Database
+const db = {
+  sequelize,
+  Sequelize,
+  models: {},
+};
+
+// Load the Movie model
+db.models.Movie = require('./models/movie.js')(sequelize);
+
+// Export the Database
+module.exports = db;
